e7cb0442b2fd3f0967187000b2cd4370fd83564b
K0966527 <phattarapong.p@kbtg.tech>
2024-02-21 06:07:18 +0700

    add file

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const lodash = require('lodash');
+const moment = require('moment');
+const axios = require('axios');
+const dotenv = require('dotenv');
+const winston = require('winston');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan('combined'));
+
+app.get('/', (req, res) => {
+    const message = lodash.capitalize('hello world');
+    const time = moment().format('MMMM Do YYYY, h:mm:ss a');
+    res.send(`${message}! Current server time is ${time}.`);
+});
+
+app.listen(3000, () => {
+    winston.log('info', 'Server is running on port 3000');
+});
